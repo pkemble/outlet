d10d1d622be1b9153c4301529ec60e4482488f08
Pete <pkemble@gmail.com>
2021-03-22 09:59:26 -0400

    cap

--- config/deploy.rb
+++ config/deploy.rb
@@ -22,7 +22,7 @@ set :deploy_to, "/var/www/outlet"
 # set :pty, true
 
 # Default value for :linked_files is []
-append :linked_files, %w{config/master.key config/.db}
+append :linked_files, "config/master.key", "config/.db"
 
 # Default value for linked_dirs is []
 # append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
