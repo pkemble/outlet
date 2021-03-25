fd6cd4ea2260fbb7d79854b684505f59a09ad9b3
Pete <pkemble@gmail.com>
2021-03-24 21:12:02 -0400

    fixed devise root sign in

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,10 @@ Rails.application.routes.draw do
 	authenticated :user do
 	  root to: 'posts#index', as: :authenticated_root
 	end
-	root to: redirect('/users/sign_in')
+  
+	devise_scope :user do
+    root to: "devise/sessions#new"
+  end
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
